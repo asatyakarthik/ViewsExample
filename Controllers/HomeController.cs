4af3296a96402fad122770b7b19e5ff23ecaeedd
ap\alapatis <SathyaKarthik.Alapati@landisgyr.com>
2024-12-09 16:21:33 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            //return View();//if we don't specify name then it considers view name as Index.cshtml    //views/home/index.cshtml
+            return View("Index");//here it will consider view name as abc.cshtml
+            //return new ViewResult();
+        }
+    }
+}
